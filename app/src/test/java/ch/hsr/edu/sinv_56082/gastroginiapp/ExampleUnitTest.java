952708a17670fc83556e5e598772dcaa72e822ac
tschmitz <tschmitz@hsr.ch>
2016-03-31 18:24:36 +0200

    build process

--- app/src/test/java/ch/hsr/edu/sinv_56082/gastroginiapp/ExampleUnitTest.java
+++ app/src/test/java/ch/hsr/edu/sinv_56082/gastroginiapp/ExampleUnitTest.java
@@ -1,15 +1,28 @@
 package ch.hsr.edu.sinv_56082.gastroginiapp;
 
+import com.activeandroid.ActiveAndroid;
+import com.activeandroid.query.Select;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import ch.hsr.edu.sinv_56082.gastroginiapp.domain.products.ProductCategory;
 
 import static org.junit.Assert.*;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+        ProductCategory cat = new ProductCategory();
+        cat.name = "test";
+        cat.save();
+
+        ProductCategory cat2 = new Select().from(ProductCategory.class).executeSingle();
+        assertEquals(cat.name, cat2.name);
     }
 }
