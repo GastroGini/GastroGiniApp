1d9adf94ec5389d15e058c23c19fb9ccead4b2ae
tschmitz <tschmitz@hsr.ch>
2016-04-11 09:45:08 +0200

    some unitTests

---
+++ app/src/test/java/ch/hsr/edu/sinv_56082/gastroginiapp/DomainProductCategoryTest.java
@@ -0,0 +1,49 @@
+package ch.hsr.edu.sinv_56082.gastroginiapp;
+
+import com.activeandroid.ActiveAndroid;
+import com.activeandroid.query.Select;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import ch.hsr.edu.sinv_56082.gastroginiapp.domain.UUIDModel;
+import ch.hsr.edu.sinv_56082.gastroginiapp.domain.models.ProductCategory;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class DomainProductCategoryTest {
+
+    ProductCategory test1, test2;
+
+    @Before
+    public void setUp(){
+        test1 = new ProductCategory("Test1");
+        test2 = new ProductCategory("Test2");
+        test1.save();
+        test2.save();
+    }
+
+    @Test
+    public void testQueryWithSelectByName(){
+        ProductCategory query = new Select().from(ProductCategory.class).where("name=?", test1.name).executeSingle();
+        assertEquals(query, test1);
+    }
+
+    @Test
+    public void testCreate() {
+        ProductCategory category = new ProductCategory("TestCategory");
+        category.save();
+
+        ProductCategory query = ProductCategory.load(ProductCategory.class, category.getId());
+        assertEquals(category, query);
+    }
+
+
+}
