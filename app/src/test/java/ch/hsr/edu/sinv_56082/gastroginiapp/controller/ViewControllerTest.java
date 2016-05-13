e1a630dd52597e2db4dbd60e6d59bab27b0dde69
Phil <waldi_92@hotmail.com>
2016-05-13 15:09:04 +0200

    viewcontroller test template

--- app/src/test/java/ch/hsr/edu/sinv_56082/gastroginiapp/controller/ViewControllerTest.java
+++ app/src/test/java/ch/hsr/edu/sinv_56082/gastroginiapp/controller/ViewControllerTest.java
@@ -1,11 +1,19 @@
 package ch.hsr.edu.sinv_56082.gastroginiapp.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.annotation.Config;
 
+import java.util.UUID;
+
 import ch.hsr.edu.sinv_56082.gastroginiapp.BuildConfig;
+import ch.hsr.edu.sinv_56082.gastroginiapp.Helpers.Supplier;
+import ch.hsr.edu.sinv_56082.gastroginiapp.TestDataSetup;
+import ch.hsr.edu.sinv_56082.gastroginiapp.controllers.view.ViewController;
+import ch.hsr.edu.sinv_56082.gastroginiapp.domain.models.Product;
+import ch.hsr.edu.sinv_56082.gastroginiapp.domain.models.ProductDescription;
 
 import static org.junit.Assert.*;
 
@@ -13,9 +21,41 @@ import static org.junit.Assert.*;
 @Config(constants = BuildConfig.class, sdk = 21)
 public class ViewControllerTest {
 
-    @Test
-    public void test(){
-        assertTrue(true);
+    private TestDataSetup tds;
+
+    @Before
+    public void setup(){
+        tds = new TestDataSetup();
     }
 
+    @Test
+    public void testGetModelList(){
+
+    }
+    @Test
+    public void testGetUUID(){
+        String productUUID;
+        ViewController<Product> vc=new ViewController(Product.class);
+        productUUID = vc.get(tds.prod.getUuid());
+    }
+    @Test
+    public void testGetString(){
+
+    }
+    @Test
+    public void testCreate(){
+
+    }
+    @Test
+    public void testPrepare(){
+
+    }
+    @Test
+    public void testUpdate(){
+
+    }
+    @Test
+    public void testDelete(){
+
+    }
 }
