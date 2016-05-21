48a8cb182193b5dadc3bc56ef325c75b32c67f1a
Zmote <shodaime.hikarikage@hotmail.com>
2016-05-21 23:55:31 +0200

    further tests added

--- app/src/test/java/ch/hsr/edu/sinv_56082/gastroginiapp/ui/activities/HomeScreenActivityTest.java
+++ app/src/test/java/ch/hsr/edu/sinv_56082/gastroginiapp/ui/activities/HomeScreenActivityTest.java
@@ -3,32 +3,58 @@ package ch.hsr.edu.sinv_56082.gastroginiapp.ui.activities;
 
 import android.widget.Button;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
 import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowApplication;
+import static org.robolectric.Shadows.shadowOf;
 
 import ch.hsr.edu.sinv_56082.gastroginiapp.BuildConfig;
 import ch.hsr.edu.sinv_56082.gastroginiapp.R;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class HomeScreenActivityTest {
+    HomeScreenActivity activity;
+    Button eventButton;
+    Button menuButton;
+
+    @Before
+    public void setUp() {
+        activity = Robolectric.setupActivity(HomeScreenActivity.class);
+        eventButton = (Button) activity.findViewById(R.id.eventButton);
+        menuButton = (Button) activity.findViewById(R.id.menuButton);
+
+    }
 
     @Test
     public void testSetup(){
-        HomeScreenActivity activity = Robolectric.setupActivity(HomeScreenActivity.class);
-
-
-        Button eventButton = (Button) activity.findViewById(R.id.eventButton);
-        Button menuButton = (Button) activity.findViewById(R.id.menuButton);
-
         assertEquals(eventButton.getText().toString(), activity.getResources().getString(R.string.events));
         assertEquals(menuButton.getText().toString(), activity.getResources().getString(R.string.menu));
+    }
 
+    @Test
+    public void eventButtonCallsNextActivity(){
+        eventButton.performClick();
+        ShadowApplication application = shadowOf(RuntimeEnvironment.application);
+        assertThat("Next activity has started", application.getNextStartedActivity(), is(notNullValue()));
+    }
+
+    @Test
+    public void menuButtonCallsNextActivity(){
+        menuButton.performClick();
+        ShadowApplication application = shadowOf(RuntimeEnvironment.application);
+        assertThat("Next activity has started", application.getNextStartedActivity(), is(notNullValue()));
     }
 
 }
--- app/src/test/java/ch/hsr/edu/sinv_56082/gastroginiapp/ui/activities/menu/MenuMainTest.java
+++ app/src/test/java/ch/hsr/edu/sinv_56082/gastroginiapp/ui/activities/menu/MenuMainTest.java
@@ -15,24 +15,29 @@ import org.robolectric.RobolectricGradleTestRunner;
 import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowApplication;
+import static org.robolectric.Shadows.shadowOf;
 import org.robolectric.Robolectric;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
-import static org.robolectric.Shadows.shadowOf;
+import static org.junit.Assert.assertEquals;
+
 
 import ch.hsr.edu.sinv_56082.gastroginiapp.R;
+import dalvik.annotation.TestTargetClass;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk = 21)
 public class MenuMainTest {
     Button menuCardButton;
+    Button productsButton;
     Activity activity;
 
     @Before
     public void setUp() {
         activity = Robolectric.setupActivity(MenuMain.class);
         menuCardButton = (Button) activity.findViewById(R.id.menuCardButton);
+        productsButton = (Button) activity.findViewById(R.id.productsButton);
 
     }
 
@@ -41,6 +46,23 @@ public class MenuMainTest {
 
         menuCardButton.performClick();
 
+        ShadowApplication application = shadowOf(RuntimeEnvironment.application);
+        assertThat("Next activity has started", application.getNextStartedActivity(), is(notNullValue()));
+    }
+
+    @Test
+    public void productsButtonIsSetTest() {
+        assertEquals("Produkte",productsButton.getText().toString());
+    }
+
+    @Test
+    public void menuCardButtonIsSetTest(){
+        assertEquals("Menükartenliste",menuCardButton.getText().toString());
+    }
+
+    @Test
+    public void goToProductListTest(){
+        productsButton.performClick();
         ShadowApplication application = shadowOf(RuntimeEnvironment.application);
         assertThat("Next activity has started", application.getNextStartedActivity(), is(notNullValue()));
     }
